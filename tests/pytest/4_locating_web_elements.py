2041360916d15c3bbeb057da17008a0ec6c8c3f3
Himanshu Jagdish Sheth <himanshu.sheth@gmail.com>
2024-07-10 23:48:40 +0530

    Addition of tests for locating web elements

--- tests/pytest/4_locating_web_elements.py
+++ tests/pytest/4_locating_web_elements.py
@@ -50,11 +50,69 @@ class TestSeleniumPlayground:
             size_of_list = len(elements_list)
             print("Size of the product list:", size_of_list)
 
-            driver.get(locators.test_sel_input_playground_url)
+            # Locating elements by TAG NAME
+            elements_list = locate_elements(driver, By.TAG_NAME, locators.tagHeading)
 
+            size_of_list = len(elements_list)
+            print("Size of the element with tag h4:", size_of_list)
+
+            print("Value of the first element with tag h4: " + elements_list[0].text)
+
+            # Printing text of all the items in the list
+            count = 0
+            for element in elements_list:
+                count += 1
+                print("Value of the element " + str(count) + " with tag h4: " + element.text)
+
+            # Locating elements by LINK TEXT
+            driver.get(locators.test_sel_input_playground_url)
+            elements_list = locate_elements(driver, By.LINK_TEXT, locators.linkPricing)
+            if elements_list:
+                elements_list[0].click()
+
+            # Assert if the page title does not match the expected one
+            try:
+                page_title = driver.title
+                assert locators.linkPricingTitle == page_title, f"Expected title '{locators.linkPricingTitle}' matches with'{page_title}'"
+                print(f"Expected title '{locators.linkPricingTitle}' matches with'{page_title}'")
+                if exec_platform == 'cloud':
+                    driver.execute_script('lambda-status=passed')
+            except AssertionError:
+                if exec_platform == 'cloud':
+                    driver.execute_script('lambda-status=failed')
+                pytest.fail(f"Expected title '{locators.linkPricingTitle}'does not match with '{page_title}'")
+
+            time.sleep(2)
+
+            # Locating elements by PARTIAL LINK TEXT
+            driver.get(locators.test_sel_playground_url)
+            elements_list = locate_elements(driver, By.PARTIAL_LINK_TEXT, locators.linkPartialDown)
+
+            # Printing text of all the items in the list
+            count = 0
+            for element in elements_list:
+                count += 1
+                print("Href element " + str(count) + " with tag h4: " \
+                      + element.get_attribute('href'))
+
+            if elements_list:
+                elements_list[2].click()
+
+            # Assert if the page title does not match the expected one
+            try:
+                element = locate_element(driver, By.CSS_SELECTOR, "#downloadButton")
+                print(f"Pass: Page Title: '{driver.title}'")
+                if exec_platform == 'cloud':
+                    driver.execute_script('lambda-status=passed')
+            except NoSuchElementException:
+                if exec_platform == 'cloud':
+                    driver.execute_script('lambda-status=failed')
+                pytest.fail(f"Fail: Page Title: '{driver.title}'")
+
+            time.sleep(2)    
 
         except Exception as e:
             # Catch other exceptions
-            print(f"Failed: Input Form Demo, generic exception - {e}")
+            print(f"Failed: Locating web elements, generic exception - {e}")
             if exec_platform == 'cloud':
                 driver.execute_script("lambda-status=failed")
